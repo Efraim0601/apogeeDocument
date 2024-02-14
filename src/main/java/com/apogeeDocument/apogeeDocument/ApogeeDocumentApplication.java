42a1a69bbb1f88f516c9d70a5fa0041d39db65ce
Efraim <takouaefraim@gmail.com>
2024-02-14 13:54:53 +0100

    Initial commit

---
+++ src/main/java/com/apogeeDocument/apogeeDocument/ApogeeDocumentApplication.java
@@ -0,0 +1,13 @@
+package com.apogeeDocument.apogeeDocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApogeeDocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApogeeDocumentApplication.class, args);
+	}
+
+}
